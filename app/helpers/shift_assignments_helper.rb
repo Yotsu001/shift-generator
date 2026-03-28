acc44551c2b46611a4927989a11d1c47a6db46cf
Yotsu001 <re.af.sna.g7n.9@gmail.com>
2026-03-28 23:43:17 +0900

    日本語化

--- app/helpers/shift_assignments_helper.rb
+++ app/helpers/shift_assignments_helper.rb
@@ -1,31 +1,19 @@
 module ShiftAssignmentsHelper
   def work_type_label(work_type)
-    case work_type
-    when "day_shift"
-      "日勤"
-    when "night_shift"
-      "夜勤"
-    when "off_duty"
-      "非番"
-    when "holiday"
-      "休日"
-    else
-      work_type
-    end
+    I18n.t("enums.shift_assignment.work_type.#{work_type}", default: work_type)
   end
 
   def day_type_label(day_type)
-    case day_type
-    when "weekday"
-      "平日"
-    when "saturday"
-      "土曜"
-    when "sunday"
-      "日曜"
-    when "holiday"
-      "祝日"
-    else
-      day_type
+    I18n.t("enums.shift_day.day_type.#{day_type}", default: day_type)
+  end
+
+  def status_label(status)
+    I18n.t("enums.shift_period.status.#{status}", default: status)
+  end
+
+  def work_type_options
+    ShiftAssignment.work_types.keys.map do |key|
+      [work_type_label(key), key]
     end
   end
 

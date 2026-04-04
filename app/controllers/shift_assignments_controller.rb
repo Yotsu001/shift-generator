ffbc8f5af095bb6f86f3abfbbaf6277187a839a9
Yotsu001 <re.af.sna.g7n.9@gmail.com>
2026-04-04 19:53:47 +0900

    トグルの表示改善

--- app/controllers/shift_assignments_controller.rb
+++ app/controllers/shift_assignments_controller.rb
@@ -1,6 +1,7 @@
 class ShiftAssignmentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_shift_day
+  before_action :set_shift_assignment, only: [:update, :destroy]
 
   def create
     @shift_assignment = @shift_day.shift_assignments.new(shift_assignment_params)
@@ -8,27 +9,26 @@ class ShiftAssignmentsController < ApplicationController
     if @shift_assignment.save
       redirect_to shift_period_path(@shift_day.shift_period), notice: "割当を登録しました。"
     else
-      redirect_to shift_period_path(@shift_day.shift_period), alert: @shift_assignment.errors.full_messages.join(", ")
+      prepare_shift_period_show_data
+      @open_form_id = "assignment-form-#{@shift_assignment.user_id}-#{@shift_day.id}"
+      render "shift_periods/show", status: :unprocessable_entity
     end
   end
 
   def update
-    @shift_assignment = @shift_day.shift_assignments.find(params[:id])
-
     if @shift_assignment.update(shift_assignment_params)
       redirect_to shift_period_path(@shift_day.shift_period), notice: "割当を更新しました。"
     else
-      redirect_to shift_period_path(@shift_day.shift_period), alert: @shift_assignment.errors.full_messages.join(", ")
+      prepare_shift_period_show_data
+      @open_form_id = "edit-assignment-form-#{@shift_assignment.id}"
+      render "shift_periods/show", status: :unprocessable_entity
     end
   end
 
-  def destroy
-    @shift_day = ShiftDay.find(params[:shift_day_id])
-    @shift_assignment = @shift_day.shift_assignments.find(params[:id])
-    @shift_assignment.destroy
-
-    redirect_to shift_period_path(@shift_day.shift_period), notice: "割当を削除しました。"
-  end
+    def destroy
+      @shift_assignment.destroy
+      redirect_to shift_period_path(@shift_day.shift_period), notice: "割当を削除しました。"
+    end
 
   private
 
@@ -36,7 +36,19 @@ class ShiftAssignmentsController < ApplicationController
     @shift_day = ShiftDay.find(params[:shift_day_id])
   end
 
+  def set_shift_assignment
+    @shift_assignment = @shift_day.shift_assignments.find(params[:id])
+  end
+
   def shift_assignment_params
-    params.require(:shift_assignment).permit(:user_id, :zone_id, :work_type)
+    params.require(:shift_assignment).permit(:user_id, :work_type, :zone_id)
+  end
+
+  def prepare_shift_period_show_data
+    @shift_period = @shift_day.shift_period
+    @shift_days = @shift_period.shift_days
+                              .includes(:leave_requests, shift_assignments: [:user, :zone])
+                              .order(:target_date)
+    @users = User.includes(:zones).order(:id)
   end
 end
